c8f1a4f3bdfc4320877d0be72e2237cdafd38950
NeNeTheCreative <shecodes@nenethecreative.com>
2021-07-23 06:35:29 -0400

    task 1 completed

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
+  `nestedFunction() is a closure which means nestedFunction() can reach out of it's scope to grab a variable defined in an outer function`
+
 
 
 
@@ -28,8 +30,9 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+function summation(num) {
+  for (let i=0; 1 < num; 1 ++)
+  
 
   }
  
